c27c5eabac9415bec1874baa5947a6779456ca8d
hypermail <hypermail@yandex.ru>
2022-12-20 20:01:45 +0300

    lesson_2 Static site with Django

--- BraniacLMS/config/settings.py
+++ BraniacLMS/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-60bxitg5!gn*&)qp@w(6j&)p6811))na2wmn^g51n=ojhia_hw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
