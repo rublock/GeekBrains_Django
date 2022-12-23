a8ec122be5370c2f4465f3f43b9551a609e8a1bc
hypermail <hypermail@yandex.ru>
2022-12-23 13:01:07 +0300

    lesson_3 Loops in templates

--- BraniacLMS/mainapp/views.py
+++ BraniacLMS/mainapp/views.py
@@ -9,9 +9,14 @@ class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
+        # Get all previous data
         context = super().get_context_data(**kwargs)
+        # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
         return context
 
 
