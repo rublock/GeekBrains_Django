2b4541de8b154e643ba3e5e0e6e9840f9579e38c
hypermail <hypermail@yandex.ru>
2022-12-25 15:48:14 +0300

    lesson_3 Args in url

--- BraniacLMS/mainapp/urls.py
+++ BraniacLMS/mainapp/urls.py
@@ -6,10 +6,11 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.MainPageView.as_view(), name='index'),
-    path("news/", views.NewsPageView.as_view(), name='news'),
-    path("courses/", views.CoursesPageView.as_view(), name='courses'),
-    path("contacts/", views.ContactsPageView.as_view(), name='contacts'),
-    path("doc_site/", views.DocSitePageView.as_view(), name='doc_site'),
-    path("login/", views.LoginPageView.as_view(), name='login'),
+    path("", views.MainPageView.as_view(), name="index"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
+    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
--- BraniacLMS/mainapp/views.py
+++ BraniacLMS/mainapp/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import TemplateView
 from datetime import datetime
+
+from django.views.generic import TemplateView
 
 
 class MainPageView(TemplateView):
@@ -17,6 +18,13 @@ class NewsPageView(TemplateView):
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         context["range"] = range(5)
         context["datetime_obj"] = datetime.now()
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
         return context
 
 
